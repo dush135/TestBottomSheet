3267200ea3822cd8d5c0a3e0659aa508623a44ba
dushan135.dev <dushan135.dev@outlook.com>
2021-09-03 22:33:28 +0530

    Add project files.

---
+++ TestBottomSheet/TestBottomSheet/ItemData.cs
@@ -0,0 +1,9 @@
+﻿namespace TestBottomSheet
+{
+    public class ItemData
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ TestBottomSheet/TestBottomSheet/MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestBottomSheet
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public List<ItemData> itemList = new List<ItemData>();
+        private double y;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync("https://jsonplaceholder.typicode.com/albums");
+
+            if (json !=null) {
+
+                itemList = JsonConvert.DeserializeObject<List<ItemData>>(json);
+                ItemListView.ItemsSource = itemList;
+                ItemListView2.ItemsSource = itemList;
+            }
+
+        }
+
+        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            // Handle the pan
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    // Translate and ensure we don't y + e.TotalY pan beyond the wrapped user interface element bounds.
+                    var translateY = Math.Max(Math.Min(0, y + e.TotalY), -Math.Abs((Height * .25) - Height));
+                    bottomSheet.TranslateTo(bottomSheet.X, translateY, 20);
+                    break;
+                case GestureStatus.Completed:
+                    // Store the translation applied during the pan
+                    y = bottomSheet.TranslationY;
+
+                    //at the end of the event - snap to the closest location
+                    var finalTranslation = Math.Max(Math.Min(0, -1000), -Math.Abs(getClosestLockState(e.TotalY + y)));
+
+                    //depending on Swipe Up or Down - change the snapping animation
+                    if (isSwipeUp(e))
+                    {
+                        bottomSheet.TranslateTo(bottomSheet.X, finalTranslation, 250, Easing.SpringIn);
+                    }
+                    else
+                    {
+                        bottomSheet.TranslateTo(bottomSheet.X, finalTranslation, 250, Easing.SpringOut);
+                    }
+
+                    //dismiss the keyboard after a transition
+                    //SearchBox.Unfocus();
+                    y = bottomSheet.TranslationY;
+
+                    break;
+
+            }
+
+        }
+
+        public bool isSwipeUp(PanUpdatedEventArgs e)
+        {
+            if (e.TotalY < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //TO-DO: Make this cleaner
+        public double getClosestLockState(double TranslationY)
+        {
+            //Play with these values to adjust the locking motions - this will change depending on the amount of content ona  apge
+            var lockStates = new double[] { 0, .5, .85 };
+
+            //get the current proportion of the sheet in relation to the screen
+            var distance = Math.Abs(TranslationY);
+            var currentProportion = distance / Height;
+
+            //calculate which lockstate it's the closest to
+            var smallestDistance = 10000.0;
+            var closestIndex = 0;
+            for (var i = 0; i < lockStates.Length; i++)
+            {
+                var state = lockStates[i];
+                var absoluteDistance = Math.Abs(state - currentProportion);
+                if (absoluteDistance < smallestDistance)
+                {
+                    smallestDistance = absoluteDistance;
+                    closestIndex = i;
+                }
+            }
+
+            var selectedLockState = lockStates[closestIndex];
+            var TranslateToLockState = getProportionCoordinate(selectedLockState);
+
+            return TranslateToLockState;
+        }
+
+        public double getProportionCoordinate(double proportion)
+        {
+            return proportion * Height;
+        }
+
+        void dismissBottomSheet()
+        {
+
+            //SearchBox.Unfocus();
+            var finalTranslation = Math.Max(Math.Min(0, -1000), -Math.Abs(getProportionCoordinate(0)));
+            bottomSheet.TranslateTo(bottomSheet.X, finalTranslation, 450, Easing.SpringOut);
+            //SearchBox.Text = string.Empty;
+        }
+
+        void openBottomSheet()
+        {
+            var finalTranslation = Math.Max(Math.Min(0, -1000), -Math.Abs(getProportionCoordinate(.85)));
+            bottomSheet.TranslateTo(bottomSheet.X, finalTranslation, 150, Easing.SpringIn);
+        }
+
+    }
+
+}
